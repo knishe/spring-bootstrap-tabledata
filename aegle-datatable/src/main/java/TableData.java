0a43313d87cf27dbc0e36de9a9939ef98601e7a3
Nishanthan <nishantan.krishnakumar@hpe.com>
2017-08-21 12:14:59 +0800

    Introduced table data to hold the data table with rows and the total number of rows.

---
+++ aegle-datatable/src/main/java/TableData.java
@@ -0,0 +1,33 @@
+import java.util.Collection;
+
+/**
+ * @author Nishanthan Krishnakumar
+ * @version 1.0
+ * @since 1.0
+ */
+public class TableData<T> {
+    private Collection<T> rows;
+    private Long total;
+
+    public TableData(Collection<T> rows, Long total) {
+        this.rows = rows;
+        this.total = total;
+    }
+
+    public Collection<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(Collection<T> rows) {
+        this.rows = rows;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+}
+
